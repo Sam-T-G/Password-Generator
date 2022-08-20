01d7c8b3cab0f21a1e5b089e12213c5a41ca6f61
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-08-19 19:28:45 -0700

    Adjusted js file to meet proper semantic standards

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,53 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-var length = Number(
-  prompt(
-    "How many characters will your password be? Enter a number between 8 and 128"
-  )
-);
-
-//ask for character type
-var charType = prompt(
-  "Enter a character type: special, numeric, uppercase, lowercase."
-);
-
 //generate password
 function generatePassword() {
   //evaluate character type
+  //ask for character type
+  var charType = prompt(
+    "Enter a character type: special, numeric, uppercase, lowercase."
+  );
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowercase
+  var number = "0123456789"
+  var special = [
+    "!",
+    "\u0022",
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "\u005B",
+    "\u005C",
+    "\u005D",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  
+  var length = Number(
+    prompt(
+      "How many characters will your password be? Enter a number between 8 and 128"
+    )
+  );
   var charSet = "";
   var charTypeLower = charType.toLowerCase();
   if (charTypeLower === "lowercase") {
@@ -42,10 +67,22 @@ function generatePassword() {
   }
   return retVal;
 }
-alert(generatePassword());
 
-//make password appear in display box
-document.getElementById("display").value = password;
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// //make password appear in display box
+// document.getElementById("display").value = password;
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // var password = document.getElementById("password");
 
@@ -66,6 +103,3 @@ document.getElementById("display").value = password;
 //   copyText.select();
 //   document.execCommand("copy");
 // }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
